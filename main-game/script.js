1add8d823537335be5523409c1013f1b5788dd87
KariminK <103779342+KariminK@users.noreply.github.com>
2023-02-13 21:09:04 +0100

    Final commit

--- main-game/script.js
+++ main-game/script.js
@@ -1,5 +1,11 @@
 /** @type {HTMLCanvasElement} */
 const canvas = document.querySelector("#canvas1");
+const infoImg = document.querySelector("#infoImg");
+const nameTxt = document.querySelector("#name");
+const seasonTxt = document.querySelector("#season");
+const whereToFindTxt = document.querySelector("#whereToFind");
+const poisonTxt = document.querySelector("#poison");
+const infoContainer = document.querySelector(".infobox");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight
 const ctx = canvas.getContext("2d");
@@ -10,6 +16,7 @@ let pressD = false;
 let timeToNewEnemy = 2500;
 let gameover = false;
 let score = 0;
+let acceleration = 5;
 let lifeCount = 3;
 let enemies = [];
 
@@ -18,39 +25,75 @@ const ScoreUp = new Audio("assets/Coin01.mp3");
 const ScoreDown = new Audio("assets/Coin02.mp3");
 const gameOverSound = new Audio("assets/Alarm.mp3");
 const enemyImgs = [{
+    name: "Borowik Królewski",
+    season: "Borowik królewski najczęściej produkuje owocniki od sierpnia do listopada, wyjątkowo już w maju lub czerwcu.",
+    whereToFind: "Znaleźć go można w Europie, Chinach, Korei, Japonii i USA. W całej Europie borowik królewski jest niezwykle rzadki. W Polsce otrzymał status E – krytycznie zagrożonego wymarciem. Ciepłe lasy liściaste na podłożach alkalicznych. ",
     src: "assets/enemies/borowikKrolewski.png",
     poison: false,
 }, {
+    name: "Borowik Szatański",
+    season: "Od lipca do września.",
+    whereToFind: "Zdarza się na południu kraju, na ciepłych, widnych stanowiskach, głównie pod dębami i bukami.",
     src: "assets/enemies/borowikSzatanski.png",
     poison: true,
 }, {
+    name: "Borowik Szlachetny",
+    season: "Od czerwca do października.",
+    whereToFind: "Najczęściej w pobliżu świerków, sosen, buków czy też dębów. Najliczniej występują w górskich lasach świerkowych.",
     src: "assets/enemies/borowikSzlachetny.png",
     poison: false,
 }, {
+    name: "Czubajka Czerwieniejąca",
+    season: "Czubajka czerwieniejąca rośnie w sezonie od czerwca do listopada.",
+    whereToFind: "Czubajka czerwieniejąca rośnie we wszystkich typach lasów. Najczęściej spotkać tego grzyba można w ciepłych i suchych miejscach - w parkach, na polanach. ",
     src: "assets/enemies/czubajkaCzerwieniejaca.png",
     poison: false,
 }, {
+    name: "Hełmówka Obrzezona",
+    season: "Od sierpnia do października.",
+    whereToFind: "Najczęściej w pobliżu świerków, sosen, buków czy też dębów. Najliczniej występują w górskich lasach świerkowych.",
     src: "assets/enemies/helmowkaObrzezona.png",
     poison: true,
 }, {
+    name: "Koźlarz Babka",
+    season: "Od lipca do listopada.",
+    whereToFind: "To popularne grzyby jadalne w Polsce. Lubią niezbyt wilgotne podłoże, a doświadczeni grzybiarze doskonale wiedzą, że można je znaleźć wyłącznie blisko brzóz, z którymi żyją w pełnej symbiozie. I w pewnym sensie się do nich upodabniają.",
     src: "assets/enemies/kozlarzBabka.png",
     poison: false,
 }, {
+    name: "Maślak Zwyczajny",
+    season: "Od maja do listopada.",
+    whereToFind: "To popularne grzyby jadalne, występują powszechnie w całej Polsce z wyjątkiem wyższych partii górskich. Współpracują wyłącznie z sosnami, należy więc ich szukać w pobliżu tych drzew.",
     src: "assets/enemies/maslakZwyczajny.png",
     poison: false,
 }, {
+    name: "Muchomor Plamisty",
+    season: "Muchomory plamiste rosną od lipca do października.",
+    whereToFind: "Niestety to grzyb pospolity, najbardziej przyjaźni się z dębami, bukami, sosnami i świerkami.",
     src: "assets/enemies/muchomorPlamisty.png",
     poison: true,
 }, {
+    name: "Muchomor Zielonawy",
+    season: "Muchomory sromotnikowe rosną od lipca do października.",
+    whereToFind: "W naszych lasach niestety występuje powszechnie, wyrasta zwykle pod dębami, bukami, grabami i sosnami, pojedynczo lub w grupkach.",
     src: "assets/enemies/muchomorZielonawy.png",
     poison: true,
 }, {
+    name: "Pieprznik Jadalny",
+    season: "Od maja do października.",
+    whereToFind: "Pieprznik to dość powszechny i „towarzyski” grzyb. Lubi sąsiedztwo różnych drzew, szczególnie sosen, świerków, dębów, buków i grabów. Jeśli wypatrzysz jednego, rozejrzyj się wokół. Pieprzniki rosną w grupach i często tworzą tzw. czarcie kręgi",
     src: "assets/enemies/PieprznikJadalny.png",
     poison: false,
 }, {
+    name: "Piestrzenica Kasztanowata",
+    season: "Od marca do maja.",
+    whereToFind: "Występuje głównie pod świerkami i sosnami, na podłożu piaszczystym, często na gołej ziemi, zwykle w pobliżu resztek drewna i kory.",
     src: "assets/enemies/PiestrzenicaKasztanowata.png",
     poison: true,
 }, {
+    name: "Strzępiak Ceglasty",
+    season: "Od czerwca do września.",
+    whereToFind: "Wyrasta na wapiennym podłożu, głównie pod dębami, bukami, lipami i grabami, a także w ogrodach i parkach, na przydrożach.",
     src: "assets/enemies/strzepiakCeglasty.png",
     poison: true,
 },];
@@ -67,9 +110,12 @@ class Enemy{
         this.image = new Image();
         this.image.src = enemyImgs[this.mushroomIndex].src;
         this.poison = enemyImgs[this.mushroomIndex].poison;
+        this.name = enemyImgs[this.mushroomIndex].name;
+        this.season = enemyImgs[this.mushroomIndex].season;
+        this.whereToFind = enemyImgs[this.mushroomIndex].whereToFind;
         this.x = Math.random()*(canvas.width-this.size);
         this.y = 0-this.size;
-        this.speed = 5;
+        this.speed = acceleration;
         this.readyForDelete = false;
     }
     update(){
@@ -103,12 +149,23 @@ class Player{
         for (let i = 0; i < enemies.length; i++) {
             const e = enemies[i];
             if(e.x < this.x + this.width && e.x + e.size > this.x && e.y+e.size > this.y && e.y < this.y + this.width){
+                infoContainer.classList.remove("hide");
+                nameTxt.innerText = e.name;
+                seasonTxt.innerText = e.season;
+                whereToFindTxt.innerText = e.whereToFind;
+                infoImg.src = enemyImgs[e.mushroomIndex].src;
                 if (e.poison) {
                     ScoreDown.play();
                     lifeCount--;
+                    poisonTxt.classList.remove("no");
+                    poisonTxt.innerText = "Tak"
+                    poisonTxt.classList.add("yes");
                 }else{
                     ScoreUp.play();
                     score++;
+                    poisonTxt.classList.remove("yes");
+                    poisonTxt.innerText = "Nie";
+                    poisonTxt.classList.add("no");
                 }
                 delete enemies[i];
             };
@@ -187,6 +244,7 @@ const life1 = new life();
 
 setInterval(()=>{
     enemies.push(new Enemy());
+    acceleration+= 0.5;
     timeToNewEnemy -= 100;
 }, timeToNewEnemy);
 const animate = ()=>{
